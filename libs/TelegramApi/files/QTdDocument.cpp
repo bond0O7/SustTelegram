6ce9b8ffaea12f607a3ebcadb3c381547b96e6ff
bond0O7 <saskeko1483@gmail.com>
2020-12-05 20:35:20 +0200

    Add QTdDocument

--- libs/TelegramApi/files/QTdDocument.cpp
+++ libs/TelegramApi/files/QTdDocument.cpp
@@ -1,3 +1,11 @@
 #include "QTdDocument.hpp"
 
 QTdDocument::QTdDocument(QObject *parent) : QTdObject(parent) {}
+
+QString QTdDocument::fileName() const {}
+
+QString QTdDocument::mimeType() const {}
+
+QTdPhotoSize *QTdDocument::thumbnail() const {}
+
+QTdFile *QTdDocument::document() const {}
